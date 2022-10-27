4b11535b6339e9b05a78b1eff4094d6f0ac63747
joe953 <joemazloum953@gmail.com>
2022-10-27 18:46:58 +0300

    main

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
 
-  root "users#index"
+  get '/register', to:'users#new', as:'register'
+  get '/login', to:'session#new', as:'login'
 
+  root 'static_pages#home'
 
 
 end
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/assets/stylesheets/*.{html,css,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
